eccdff0af924b96cea456325709e4bd44a3bc5cd
Traap <gary.a.howard@mac.com>
2023-03-05 10:55:29 -0800

    Add Fibonacci sequence done in Rust.

---
+++ lsp/sample/src/main.rs
@@ -0,0 +1,13 @@
+fn fibonacci(n: u32) -> u32 {
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => fibonacci(n - 1) + fibonacci(n - 2),
+    }
+}
+
+fn main() {
+    // Shoutout to Brian Kernighan
+    println!("Hello World!");
+    println!("{}", fibonacci(10));
+}
