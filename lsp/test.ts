f3e9d2f931418b8d0bb84fd1947798b8e8e1d8cb
Traap <gary.a.howard@mac.com>
2023-03-05 13:28:45 -0800

    Add Fibonacci sequence done in Typescipt.

--- lsp/test.ts
+++ lsp/test.ts
@@ -1,5 +1,16 @@
-// Shoutout to Brian Kernighan
-let message: string = "Hello, World!";
-console.log(message);
+// Shoutout to Brian Kernighan and Fibonacci sequence using recursion in
+// Typescipt.
+function fibonacci(n: number): number {
+  if (n <= 1) {
+    return n;
+  }
 
-// TODO: Fibonacci sequence done with TypeScript.
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log("Hello World!");
+console.log("Fibonacci sequence use recursion in Typescript:");
+for (let i = 0; i <= 10; i++) {
+  process.stdout.write(fibonacci(i) + ' ');
+}
+console.log('');
