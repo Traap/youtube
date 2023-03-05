3e3314c7e2a0490719514e8a3ad34201b77e6b54
Traap <gary.a.howard@mac.com>
2023-03-05 12:23:25 -0800

    Fibonacci sequence using recursion in Ruby.

--- lsp/test.rb
+++ lsp/test.rb
@@ -1,11 +1,18 @@
-# Shoutout to Brian Kernighan
-puts "Hello World!"
+# frozen_string_literal: true
 
-# Fibonacci sequence done with Ruby.
-def fibonacci(n)
-  return n if n <= 1
-  fibonacci(n-1) + fibonacci(n-2)
+# Shoutout to Brian Kernighan and Fibonacci sequence using recursion in Ruby.
+
+def fibonacci(nbr)
+  return nbr if nbr <= 1
+
+  fibonacci(nbr - 1) + fibonacci(nbr - 2)
 end
 
-# Calculate the Fibonacci sequence.
-puts fibonacci(10)
+puts 'Hello World!'
+
+puts 'Fibonacci sequence usng recursion in Ruby:'
+
+11.times do |idx|
+  print "#{fibonacci(idx)} "
+end
+puts
