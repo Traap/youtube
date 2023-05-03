1b4e59afe31625bb0ba5c2b516c436fb0a6e4cb0
Traap <gary.a.howard@mac.com>
2023-05-03 08:32:21 -0700

    ES14 example.

--- lsp/test.ts
+++ lsp/test.ts
@@ -18,3 +18,11 @@ for (let i = 0; i <= FIBONACCI_MAX; i++) {
   console.log(i + ' : ' + fibonacci(i));
 }
 console.log('');
+
+// What to expect form ECMAScript 2023 (ES14)
+// https://www.makeuseof.com/ecmascript-2023-es14-what-to-expect/?newsletter_popup=1
+// const result = [1, 2, 3, 4, 5]
+//   |> ((aar) => aar.filter((n) => n % 2 === 0))
+//   |> ((aar) => aar.map((n)) => n * 2))
+//   |> ((aar) => aar.reduce((a, b) => a + b ));
+// console.log(result); // Output: 12
