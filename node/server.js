1f9bf1dc0cc003d6746c1e4d8fad060f676c78cf
Traap <gary.a.howard@mac.com>
2023-06-24 07:43:36 -0700

    Nodejs server update.

--- node/server.js
+++ node/server.js
@@ -6,10 +6,12 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  res.write("Shoutout to Brian Kernighan: Hello World!\n");
+  res.write("Fibonacci sequence using recursion in Nodejs:\n");
   res.end('Hello World\n');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
 });
+  console.log(`Server running at http://${hostname}:${port}/`);
 
